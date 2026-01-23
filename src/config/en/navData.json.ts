430bd1b75a7a25dbbff142f9233e1f64e1d82400
Totara-thib <totara-thib@gmail.com>
2026-01-23 16:27:59 +0100

    feat: add customer logos and update CookieConsent component with aria-label

--- src/config/en/navData.json.ts
+++ src/config/en/navData.json.ts
@@ -15,123 +15,23 @@ const navConfig: navItem[] = [
     text: "OpenSource CLI",
     link: "/",
   },
-  // {
-  //   text: "Overview",
-  //   link: "/overview/",
-  // },
   {
     text: "Platform",
     link: "/platform/",
   },
-
-  // mega menu
-  // {
-  //   text: "Pages",
-  //   megaMenuColumns: [
-  //     {
-  //       title: "Landing Pages",
-  //       items: [
-  //         {
-  //           text: "Landing 1",
-  //           link: "/",
-  //           icon: "tabler/star",
-  //         },
-  //         {
-  //           text: "Landing 2",
-  //           link: "/examples/landing2",
-  //           icon: "tabler/diamonds",
-  //         },
-  //         {
-  //           text: "Landing 3",
-  //           link: "/examples/landing3",
-  //           icon: "tabler/circle",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       title: "Blog",
-  //       items: [
-  //         {
-  //           text: "Blog Index 1",
-  //           link: "/blog",
-  //           icon: "tabler/pencil",
-  //         },
-  //         {
-  //           text: "Blog Index 2",
-  //           link: "/examples/blog-index-2",
-  //           icon: "tabler/pencil",
-  //         },
-  //         {
-  //           text: "Blog Post 1",
-  //           link: "/blog/tsconfig-paths-setup",
-  //           icon: "tabler/edit-circle",
-  //         },
-  //         {
-  //           text: "Blog Post 2",
-  //           link: "/examples/blog2",
-  //           icon: "tabler/edit-circle",
-  //         },
-  //         {
-  //           text: "Categories",
-  //           link: "/categories/blogging",
-  //           icon: "tabler/category",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       title: "Elements and Forms",
-  //       items: [
-  //         {
-  //           text: "MDX Pages",
-  //           link: "/elements",
-  //           icon: "tabler/wand",
-  //         },
-  //         {
-  //           text: "Contact",
-  //           link: "/contact",
-  //           icon: "tabler/address-book",
-  //         },
-  //         {
-  //           text: "Login",
-  //           link: "/login",
-  //           icon: "tabler/login",
-  //         },
-  //         {
-  //           text: "Signup",
-  //           link: "/signup",
-  //           icon: "tabler/user",
-  //         },
-  //         {
-  //           text: "Password Reset",
-  //           link: "/password-reset",
-  //           icon: "tabler/password",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       title: "Other Pages",
-  //       items: [
-  //         {
-  //           text: "Privacy Policy",
-  //           link: "/privacy-policy",
-  //           icon: "tabler/lock-square",
-  //         },
-  //         {
-  //           text: "Page not found",
-  //           link: "/not-a-link",
-  //           icon: "tabler/error-404",
-  //         },
-  //         {
-  //           text: "RSS Feed",
-  //           link: "/rss.xml",
-  //           newTab: true,
-  //           icon: "tabler/rss",
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // },
-
+  {
+    text: "Docs",
+    dropdown: [
+      {
+        text: "Platform",
+        link: "/docs/getting-started",
+      },
+      {
+        text: "OpenSource CLI",
+        link: "/docs/cli",
+      },
+    ],
+  },
   {
     text: "Blog",
     link: "/blog/",
@@ -141,20 +41,7 @@ const navConfig: navItem[] = [
     link: "https://plumber.discord.io",
     newTab: true,
   },
-  // uncomment below to add a dropdown example
-  // {
-  // 	text: "Dropdown",
-  // 	dropdown: [
-  // 		{
-  // 			text: "Elements",
-  // 			link: "/elements/",
-  // 		},
-  // 		{
-  // 			text: "Password Reset",
-  // 			link: "/password-reset/",
-  // 		},
-  // 	],
-  // },
+
 ];
 
 export default navConfig;
