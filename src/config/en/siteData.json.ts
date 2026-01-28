10d916208ab44ae17d386b0c3aa2bc422c72a50d
Totara-thib <totara-thib@gmail.com>
2026-01-28 17:46:59 +0100

    chore: remove unused images and update site logo references in components and configuration for cleaner asset management

--- src/config/en/siteData.json.ts
+++ src/config/en/siteData.json.ts
@@ -17,7 +17,7 @@ const siteData: SiteDataProps = {
 
   // default image for meta tags if the page doesn't have an image already
   defaultImage: {
-    src: "/images/site-logo.svg",
+    src: "/images/site-logo-final.svg",
     alt: "Plumber logo",
   },
 };
